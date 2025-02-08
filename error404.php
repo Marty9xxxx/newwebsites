be5b168f814f700b44c5a1234dce0b38d6af1031
Marty9xxxx <svaty.martin@seznam.cz>
2025-02-08 19:08:44 +0100

    update 404

--- error404.php
+++ error404.php
@@ -1,19 +1,5 @@
 <?php include 'header.php'; ?>
 <h1>Hledaná stránka...</h1>
 <p>...Nebyla nalezena, zkuste zadat přesný název, případně využijte hledání na webu. Máte-li pocit, že se jedná o chybu, kontaktujte svého správce webu, případně kontaktujte nás přes <a href="contact.php" target="_blank">email</a></p>
-<link rel="stylesheet" href="<?= htmlspecialchars($aktualniStyl); ?>">
-<?php
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-
-$file = "/data/data.txt";
-
-if (file_exists($file)) {
-    echo "Soubor existuje!<br>";
-    echo "Obsah souboru: " . file_get_contents($file);
-} else {
-    echo "Soubor nenalezen!";
-}
-
-?>
+<link rel="stylesheet" href="style1.css">
 <?php include 'footer.php'; ?>
