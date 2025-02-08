4b3f267abb5419803ab5ed09f09aa7a603923f82
Marty9xxxx <svaty.martin@seznam.cz>
2025-02-08 19:22:36 +0100

    uprava mapa

--- map.js
+++ map.js
@@ -4,6 +4,6 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-L.marker([50.1207, 14.4686]).addTo(map)
+L.marker([50.1, 14.45]).addTo(map)
     .bindPopup("Hlivická 118, Praha 8")
     .openPopup();
